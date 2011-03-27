15ed848dea3a62dbc7df3bfeaad48e187c31d111
Marko <markokaartinen@gmail.com>
2011-03-27 21:50:20 +0300

    Translated Anti Bot mod

--- Modifications/Very Simple Anti Bot Registration/mod_very_simple_antibot.php
+++ Modifications/Very Simple Anti Bot Registration/mod_very_simple_antibot.php
@@ -2,24 +2,24 @@
 // Language string for mod Very Simple AntiBot Registration
 
 $lang_mod_vsabr = array(
-  'Robot title'      => 'Are you human or robot?',
-  'Robot question'   => 'Please respond with a <b>number</b> to the question: <b>%s</b>',
-  'Robot info'       => 'Checking if this is requested by a real person and not an automated program.',
-  'Robot test fail'  => 'You answered incorrectly to the "Human or Robot" question, or you are a Bot!',
+  'Robot title'      => 'Ihmisyystesti?',
+  'Robot question'   => 'Vastaa seuraavaan kysymykseen <strong>numerolla</strong>: <b>%s</b>',
+  'Robot info'       => 'Tarkistamme oletko botti vai ihminen.',
+  'Robot test fail'  => 'Vastasit v&auml;&auml;rin kysymykseen, vai oletko botti!',
 );
 
 // [modif oto] [Very Simple AntiBot questions]
 $mod_vsabr_questions = array(
-  'What is two plus two?'     => 4,
-  'What is four minus one?'   => 3,
-  'What is three plus two?'   => 5,
-  'What is two times four?'   => 8,
-  'What is five minus two?'   => 3,
-  'What is six plus three?'   => 9,
-  'What is seven minus one?'  => 6,
-  'What is eight times two?'  => 16,
+  'Kaksi plus kaksi?'     => 4,
+  'Nelj&auml; miinus yksi?'   => 3,
+  'Kolme plus kaksi?'   => 5,
+  'Kaksi kertaa nelj&auml;?'   => 8,
+  'Viisi miinus kaksi?'   => 3,
+  'Kuusi plus kolme?'   => 9,
+  'Seitsem&auml;n miinus yksi?'  => 6,
+  'Kahdeksan kertaa kaksi?'  => 16,
   'What is six times two?'    => 12,
-  'What is nine minus seven?' => 2
+  'Yhdeks&auml;n miinus seitsem&auml;n?' => 2
 );
 
 ?>
