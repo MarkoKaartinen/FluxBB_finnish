f91499e5f6f07c00af16dd9c0fdf69bb5566484f
Marko <markokaartinen@gmail.com>
2011-03-28 16:06:11 +0300

    Skandit

--- Finnish/register.php
+++ Finnish/register.php
@@ -4,34 +4,34 @@
 $lang_register = array(
 
 // Miscellaneous
-'No new regs'				=>	'Keskustelualue ei ota tällä hetkellä vastaan uusia rekisteröitymisiä.',
-'Reg cancel redirect'		=>	'Rekisteröityminen peruutettu. Uudelleenohjataan ...',
+'No new regs'				=>	'Keskustelualue ei ota t&auml;ll&auml; hetkell&auml; vastaan uusia rekister&ouml;itymisi&auml;.',
+'Reg cancel redirect'		=>	'Rekister&ouml;ityminen peruutettu. Uudelleenohjataan ...',
 'Forum rules'				=>	'S&auml;&auml;nn&ouml;t',
-'Rules legend'				=>	'Sinun täytyy hyväksyä käyttöehdot reksiteröityäksesi.',
-'Registration flood'		=>	'Uusi käyttäjä on rekisteröitynyt samasta IP- osoitteesta viimeisimmän tunnin sisällä. Rekisteröidyttäessä samasta IP- osoitteesta rekisteröitymisien välillä on tunnin viive. Yritä tunnin päästä uudelleen. Pahoittelemme teille mahdollisesti aiheutunutta ylimäärästä vaivannäköä.',
-'Agree'						=>	'Hyväksy',
+'Rules legend'				=>	'Sinun t&auml;ytyy hyv&auml;ksy&auml; k&auml;ytt&ouml;ehdot reksiter&ouml;ity&auml;ksesi.',
+'Registration flood'		=>	'Uusi k&auml;ytt&auml;j&auml; on rekister&ouml;itynyt samasta IP- osoitteesta viimeisimm&auml;n tunnin sis&auml;ll&auml;. Rekister&ouml;idytt&auml;ess&auml; samasta IP- osoitteesta rekister&ouml;itymisien v&auml;lill&auml; on tunnin viive. Yrit&auml; tunnin p&auml;&auml;st&auml; uudelleen. Pahoittelemme teille mahdollisesti aiheutunutta ylim&auml;&auml;r&auml;st&auml; vaivann&auml;k&ouml;&auml;.',
+'Agree'						=>	'Hyv&auml;ksy',
 'Cancel'					=>	'Peruuta',
-'Register'					=>	'Rekisteröidy',
+'Register'					=>	'Rekister&ouml;idy',
 
 // Form validation stuff (some of these are also used in post.php)
-'Registration errors'		=>	'Rekisteröitymisen virhetilanteet',
-'Registration errors info'	=>	'Seuraavat kohdat tulee korjata ennen rekisteröitymistä:',
-'Username censor'			=>	'Annettu käyttäjänimi sisältää yhden tai useampia sanoja joita ei hyväksytä. Valitse toinen käyttäjänimi ja yritä uudelleen.',
-'Username dupe 1'			=>	'Antamasi käyttäjänimellä on jo rekisteröidytty. Valitse toinen käyttäjänimi ja yritä uudelleen.',
-'Username dupe 2'			=>	'Antamasi käyttäjänimi on liian yleinen, valitse toinen käyttäjänimi. Käyttäjänimen tulee erota vähintään yhdellä tai useammalla merkillä (a-z tai 0-9).',
-'Email not match'			=>	'Sähköpostiosoiteet eivät vastaa toisiaan. Tarkista antamasi sähköpostiosoitteet.',
+'Registration errors'		=>	'Rekister&ouml;itymisen virhetilanteet',
+'Registration errors info'	=>	'Seuraavat kohdat tulee korjata ennen rekister&ouml;itymist&auml;:',
+'Username censor'			=>	'Annettu k&auml;ytt&auml;j&auml;nimi sis&auml;lt&auml;&auml; yhden tai useampia sanoja joita ei hyv&auml;ksyt&auml;. Valitse toinen k&auml;ytt&auml;j&auml;nimi ja yrit&auml; uudelleen.',
+'Username dupe 1'			=>	'Antamasi k&auml;ytt&auml;j&auml;nimell&auml; on jo rekister&ouml;idytty. Valitse toinen k&auml;ytt&auml;j&auml;nimi ja yrit&auml; uudelleen.',
+'Username dupe 2'			=>	'Antamasi k&auml;ytt&auml;j&auml;nimi on liian yleinen, valitse toinen k&auml;ytt&auml;j&auml;nimi. K&auml;ytt&auml;j&auml;nimen tulee erota v&auml;hint&auml;&auml;n yhdell&auml; tai useammalla merkill&auml; (a-z tai 0-9).',
+'Email not match'			=>	'S&auml;hk&ouml;postiosoiteet eiv&auml;t vastaa toisiaan. Tarkista antamasi s&auml;hk&ouml;postiosoitteet.',
 
 // Registration email stuff
-'Reg email'					=>	'Kiitos rekisteröitymisestä! Salasanasi on lähetetty antamaasi sähköpostiosoitteeseen. Jos viesti ei saavu perille tarkista sähköposisi roskapsotikansio. Ongelmatilanteessa ota yhteyttä keskustelupalstan pääkäyttäjään.',
-'Reg complete'				=>	'Rekisteröityminen onnistui. Uudelleenohjataan sisäänkirjautumista varten …',
+'Reg email'					=>	'Kiitos rekister&ouml;itymisest&auml;! Salasanasi on l&auml;hetetty antamaasi s&auml;hk&ouml;postiosoitteeseen. Jos viesti ei saavu perille tarkista s&auml;hk&ouml;posisi roskapsotikansio. Ongelmatilanteessa ota yhteytt&auml; keskustelupalstan p&auml;&auml;k&auml;ytt&auml;j&auml;&auml;n.',
+'Reg complete'				=>	'Rekister&ouml;ityminen onnistui. Uudelleenohjataan sis&auml;&auml;nkirjautumista varten …',
 
 // Register info
-'Desc 1'					=>	'Rekisteröityminen antaa sinulle oikeuden useisiin toimintoihin, jotka eivät ole käytettävissä ilman rekisteröintiä. Rekisteröityminen antaa oikeuden mm. toimintoihin kuten viestien muokkaaminen ja poistaminen sekä oman viestikohtaisen allekirjoituksen laatiminen. Mikäli sinulla on kysyttävää tähän keskustelupalstaan liittyen, ota yhteyttä keskustelupalstan pääkäyttäjään.',
-'Desc 2'					=>	'Alapuolella on lomake joka tulee täyttää rekisteröitymistä varten. Kun olet rekisteröitynyt palvelun käyttäjäksi, mukauta profiilisi asetukset sopiviksi. Alla olevat syötekentät vaikuttavat vain pieneen osan niistä asetuksista joita voit muuttaa profiilissasi.',
-'Username legend'			=>	'Käyttäjänimen tulee olla  2 - 25 merkkiä pitkä. ',
-'Pass legend'				=>	'Syötä ja vahvista valitsemasi salasana',
-'Pass info'					=>	'Salasanan tulee olla ainakin 4 merkkiä pitkä. Isot ja pienet kirjaimet huomioidaan.',
-'Email info'				=>	'Sinun täytyy antaa käytössäsi oleva toimiva sähköpostiosoite. Palvelun automaattisesti luoma salasana lähetetään tähän osoitteeseen.',
-'Confirm email'				=>	'Vahvista sähköpostiosoite',
+'Desc 1'					=>	'Rekister&ouml;ityminen antaa sinulle oikeuden useisiin toimintoihin, jotka eiv&auml;t ole k&auml;ytett&auml;viss&auml; ilman rekister&ouml;inti&auml;. Rekister&ouml;ityminen antaa oikeuden mm. toimintoihin kuten viestien muokkaaminen ja poistaminen sek&auml; oman viestikohtaisen allekirjoituksen laatiminen. Mik&auml;li sinulla on kysytt&auml;v&auml;&auml; t&auml;h&auml;n keskustelupalstaan liittyen, ota yhteytt&auml; keskustelupalstan p&auml;&auml;k&auml;ytt&auml;j&auml;&auml;n.',
+'Desc 2'					=>	'Alapuolella on lomake joka tulee t&auml;ytt&auml;&auml; rekister&ouml;itymist&auml; varten. Kun olet rekister&ouml;itynyt palvelun k&auml;ytt&auml;j&auml;ksi, mukauta profiilisi asetukset sopiviksi. Alla olevat sy&ouml;tekent&auml;t vaikuttavat vain pieneen osan niist&auml; asetuksista joita voit muuttaa profiilissasi.',
+'Username legend'			=>	'K&auml;ytt&auml;j&auml;nimen tulee olla  2 - 25 merkki&auml; pitk&auml;. ',
+'Pass legend'				=>	'Sy&ouml;t&auml; ja vahvista valitsemasi salasana',
+'Pass info'					=>	'Salasanan tulee olla ainakin 4 merkki&auml; pitk&auml;. Isot ja pienet kirjaimet huomioidaan.',
+'Email info'				=>	'Sinun t&auml;ytyy antaa k&auml;yt&ouml;ss&auml;si oleva toimiva s&auml;hk&ouml;postiosoite. Palvelun automaattisesti luoma salasana l&auml;hetet&auml;&auml;n t&auml;h&auml;n osoitteeseen.',
+'Confirm email'				=>	'Vahvista s&auml;hk&ouml;postiosoite',
 
 );
