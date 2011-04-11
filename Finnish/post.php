0ebbf484500add1486a8db0843fad8d95b0d0311
Marko <markokaartinen@gmail.com>
2011-04-11 08:06:02 +0300

    Skandit muunnettu (@skvark aikavyöhykkeitä ei kannata suomentaa)

--- Finnish/post.php
+++ Finnish/post.php
@@ -4,35 +4,35 @@
 $lang_post = array(
 
 // Post validation stuff (many are similiar to those in edit.php)
-'No subject'		=>	'Ketjuilla pitää olla aihe.',
-'No subject after censoring'	=>	'Ketjuilla pitää olla aihe. Suodatuksen jälkeen aiheesi oli tyhjä.',
-'Too long subject'	=>	'Aihe ei voi olla yli 70 merkkiä pitkä.',
-'No message'		=>	'Sinun täytyy kirjoittaa viesti.',
-'No message after censoring'	=>	'Sinun täytyy kirjoittaa viesti. Suodatuksen jälkeen viestisi oli tyhjä.',
-'Too long message'	=>	'Viestit eivät voi olla %s tavua pidempiä.',
-'All caps subject'	=>	'Aiheet eivät voi sisältää ainoastaan isoja kirjaimia.',
-'All caps message'	=>	'Viestit eivät voi sisältää ainoastaan isoja kirjaimia.',
-'Empty after strip'	=>	'Näyttää siltä, että viestisi sisälsi ainoastaan tyhjiä BBCode-tageja. Voi olla, että tämä tapahtui esimerkiksi sisimmän lainauksen hylkäämisen vuoksi, koska suurin sallittu lainausten määrä ylittyi.',
+'No subject'		=>	'Ketjuilla pit&auml;&auml; olla aihe.',
+'No subject after censoring'	=>	'Ketjuilla pit&auml;&auml; olla aihe. Suodatuksen j&auml;lkeen aiheesi oli tyhj&auml;.',
+'Too long subject'	=>	'Aihe ei voi olla yli 70 merkki&auml; pitk&auml;.',
+'No message'		=>	'Sinun t&auml;ytyy kirjoittaa viesti.',
+'No message after censoring'	=>	'Sinun t&auml;ytyy kirjoittaa viesti. Suodatuksen j&auml;lkeen viestisi oli tyhj&auml;.',
+'Too long message'	=>	'Viestit eiv&auml;t voi olla %s tavua pidempi&auml;.',
+'All caps subject'	=>	'Aiheet eiv&auml;t voi sis&auml;lt&auml;&auml; ainoastaan isoja kirjaimia.',
+'All caps message'	=>	'Viestit eiv&auml;t voi sis&auml;lt&auml;&auml; ainoastaan isoja kirjaimia.',
+'Empty after strip'	=>	'N&auml;ytt&auml;&auml; silt&auml;, ett&auml; viestisi sis&auml;lsi ainoastaan tyhji&auml; BBCode-tageja. Voi olla, ett&auml; t&auml;m&auml; tapahtui esimerkiksi sisimm&auml;n lainauksen hylk&auml;&auml;misen vuoksi, koska suurin sallittu lainausten m&auml;&auml;r&auml; ylittyi.',
 
 // Posting
 'Post errors'		=>	'Viestin virheet',
-'Post errors info'	=>	'Seuraavat virheet tulee korjata ennen kuin viesti voidaan lähettää:',
+'Post errors info'	=>	'Seuraavat virheet tulee korjata ennen kuin viesti voidaan l&auml;hett&auml;&auml;:',
 'Post preview'		=>	'Viestin esikatselu',
 'Guest name'		=>	'Nimi', // For guests (instead of Username)
-'Post redirect'		=>	'Viesti lisätty. Uudelleenohjataan …',
-'Post a reply'		=>	'Lähetä viesti',
-'Post new topic'	=>	'Lähetä uusi ketju',
-'Hide smilies'		=>	'Älä koskaan näytä hymiöitä ikoneina tässä ketjussa',
-'Subscribe'			=>	'Tilaa tämä ketju',
-'Stay subscribed'	=>	'Pidä tämän ketjun tilaus',
+'Post redirect'		=>	'Viesti lis&auml;tty. Uudelleenohjataan …',
+'Post a reply'		=>	'L&auml;het&auml; viesti',
+'Post new topic'	=>	'L&auml;het&auml; uusi ketju',
+'Hide smilies'		=>	'&Auml;l&auml; koskaan n&auml;yt&auml; hymi&ouml;it&auml; ikoneina t&auml;ss&auml; ketjussa',
+'Subscribe'			=>	'Tilaa t&auml;m&auml; ketju',
+'Stay subscribed'	=>	'Pid&auml; t&auml;m&auml;n ketjun tilaus',
 'Topic review'		=>	'Ketjun esikatselu (uusin ensin)',
-'Flood start'		=>	'Vähintään',
-'flood end'			=>	'sekuntia täytyy kulua viestien välillä. Odota hetki ja yritä sitten uudelleen.',
+'Flood start'		=>	'V&auml;hint&auml;&auml;n',
+'flood end'			=>	'sekuntia t&auml;ytyy kulua viestien v&auml;lill&auml;. Odota hetki ja yrit&auml; sitten uudelleen.',
 'Preview'			=>	'Esikatselu', // submit button to preview message
 
 // Edit post
-'Edit post legend'	=>	'Muokkaa viestiä ja hyväksy muutokset',
-'Silent edit'		=>	'Hiljainen muokkaus (älä näytä "Muokattu ..." ketjun katselussa)',
+'Edit post legend'	=>	'Muokkaa viesti&auml; ja hyv&auml;ksy muutokset',
+'Silent edit'		=>	'Hiljainen muokkaus (&auml;l&auml; n&auml;yt&auml; "Muokattu ..." ketjun katselussa)',
 'Edit post'			=>	'Muokkaa',
 'Edit redirect'		=>	'Viesti&auml; p&auml;ivitetty. Ohjataan...'
 
