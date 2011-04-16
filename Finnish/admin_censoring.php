99cc5795a55c93e5cdb9c7f9e15d66edc90d6327
Marko Kaartinen <markokaartinen@gmail.com>
2011-04-15 23:50:07 -0700

    Tiedosto käännetty ja skandit muunnettu.

--- Finnish/admin_censoring.php
+++ Finnish/admin_censoring.php
@@ -3,19 +3,19 @@
 // Language definitions used in admin_censoring.php
 $lang_admin_censoring = array(
 
-'Must enter word message'	=>	'You must enter a word to censor.',
-'Word updated redirect'		=>	'Censor word updated. Redirecting …',
-'Word added redirect'		=>	'Censor word added. Redirecting …',
-'Word removed redirect'		=>	'Censor word removed. Redirecting …',
-'Censoring head'			=>	'Censoring',
-'Add word subhead'			=>	'Add word',
-'Add word info'				=>	'Enter a word that you want to censor and the replacement text for this word. Wildcards are accepted (i.e. *some* would match somewhere and lonesome). Censor words also affect usernames. New users will not be able to register with usernames containing any censored words. The search is case insensitive.',
-'Censoring enabled'			=>	'<strong>Censoring is enabled in %s.</strong>',
-'Censoring disabled'		=>	'<strong>Censoring is disabled in %s.</strong>',
-'Censored word label'		=>	'Censored word',
-'Replacement label'			=>	'Replacement word(s)',
-'Action label'				=>	'Action',
-'Edit remove subhead'		=>	'Edit or remove words',
-'No words in list'			=>	'No censor words in list.',
+'Must enter word message'	=>	'Sinun t&auml;ytyy antaa sana sensuroitavaksi.',
+'Word updated redirect'		=>	'Sensuroitu sana p&auml;ivitetty. Uudelleenohjataan...',
+'Word added redirect'		=>	'Sana lis&auml;tty. Uudelleenohjataan...',
+'Word removed redirect'		=>	'Sana poistettu. Uudelleenohjataan...',
+'Censoring head'			=>	'Sensurointi',
+'Add word subhead'			=>	'Lis&auml;&auml; sana',
+'Add word info'				=>	'Lis&auml;&auml; sana, jonka haluat sensuroida ja lis&auml;&auml; korvaava sana. "Jokerimerkit" ovat sallittuja (esim. *joku* t&auml;sm&auml;isi sanoihin jokunen ja joku). Sensuurit vaikuttavat my&ouml;s k&auml;ytt&auml;j&auml;tunnuksiin. Uudet k&auml;ytt&auml;j&auml;t eiv&auml;t voi rekister&ouml;id&auml; tunnuksia joka sis&auml;lt&auml;&auml; sensuroidun sanan. Haku ei ota huomioon kirjainten kokoa.',
+'Censoring enabled'			=>	'<strong>Sensuuri k&auml;yt&ouml;ss&auml; %s.</strong>',
+'Censoring disabled'		=>	'<strong>Sensuuri pois k&auml;yt&ouml;st&auml; %s.</strong>',
+'Censored word label'		=>	'Sensuroitu sana',
+'Replacement label'			=>	'Korvaava(t) sana(t)',
+'Action label'				=>	'Toiminto',
+'Edit remove subhead'		=>	'Muokkaa tai poista sanoja',
+'No words in list'			=>	'Ei sensuroituja sanoja.',
 
 );
