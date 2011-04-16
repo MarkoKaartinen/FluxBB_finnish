d56c3c3f2aa83b0a7124cd34c2c5dbdc7a7046d1
Marko Kaartinen <markokaartinen@gmail.com>
2011-04-15 23:42:31 -0700

    Koko tiedosto käännetty ja skandit muunnettu

--- Finnish/admin_reports.php
+++ Finnish/admin_reports.php
@@ -3,19 +3,19 @@
 // Language definitions used in admin_reports.php
 $lang_admin_reports = array(
 
-'Report zapped redirect'	=>	'Report marked as read. Redirecting …',
-'New reports head'			=>	'New reports',
-'Deleted user'				=>	'Deleted user',
-'Deleted'					=>	'Deleted',
-'Post ID'					=>	'Post #%s',
-'Report subhead'			=>	'Reported %s',
-'Reported by'				=>	'Reported by %s',
-'Reason'					=>	'Reason',
-'Zap'						=>	'Mark as read',
-'No new reports'			=>	'There are no new reports.',
-'Last 10 head'				=>	'10 last read reports',
+'Report zapped redirect'	=>	'Raportti merkitty luetuksi. Uudelleenohjataan...',
+'New reports head'			=>	'Uudet raportit',
+'Deleted user'				=>	'Poistettu k&auml;ytt&auml;j&auml;',
+'Deleted'					=>	'Poistetut',
+'Post ID'					=>	'Viesti #%s',
+'Report subhead'			=>	'Raportoitu %s',
+'Reported by'				=>	'Raportoija %s',
+'Reason'					=>	'Syy',
+'Zap'						=>	'Merkitse luetuksi',
+'No new reports'			=>	'Ei uusia raportteja.',
+'Last 10 head'				=>	'10 viimeisint&auml; luettua raporttia',
 'NA'						=>	'N/A',
-'Zapped subhead'			=>	'Marked as read %s by %s',
-'No zapped reports'			=>	'There are no read reports.',
+'Zapped subhead'			=>	'Merkitty luetuksi %s - %s',
+'No zapped reports'			=>	'Ei luettuja raportteja.',
 
 );
